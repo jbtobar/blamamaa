c3dacae719aaaa6117892f8ffb21230ba2007bb2
JBT <jbtobar.io@gmail.com>
2020-11-04 13:03:19 -0500

    Create rootReducer.ts

---
+++ rootReducer.ts
@@ -0,0 +1,13 @@
+import { combineReducers } from 'redux';
+import { connectRouter } from 'connected-react-router';
+import { History } from 'history';
+// eslint-disable-next-line import/no-cycle
+import userReducer from './slices/user';
+// import counterReducer from './features/counter/counterSlice';
+
+export default function createRootReducer(history: History) {
+  return combineReducers({
+    router: connectRouter(history),
+    user: userReducer,
+  });
+}
